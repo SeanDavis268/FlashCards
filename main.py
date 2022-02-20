8b3cfc680156668185a4c99b02e33fd8001fb411
SeanDavis268 <51014235+SeanDavis268@users.noreply.github.com>
2022-02-19 22:56:32 -0600

    Flash Card viewer
    Added buttons to the flash card viewing screen and formatting changes.

--- main.py
+++ main.py
@@ -13,11 +13,41 @@ class mainWindow():
             print("pickling")
 
         win=Tk() ##upon initiation make the start screen
+        win.geometry("500x500+300+300")
+        win.update()
         win.title("Flash Cards")
+
+        #this stuff below should be in a mthod so it can be used later
+        #NOTE also need anchored
         headerTxt = Label(win, text="Flash Cards").pack(side=TOP)
-        newSetBTN = Button(win, text="create new set").pack(side=TOP,expand=YES,fill=X)
+        newSetBTN = Button(win, text="create new set").pack(anchor=N,side=TOP,expand=YES,fill=X)
         self.populate(win)
         win.mainloop()
+
+
+
+    #####
+    def openSet(self,setName,setDict,root):
+        """ when a set button is clicked this clears the screen and creates
+            1. return arrow 2. title of set, 3. plus and minus to create and
+            remove cards from the current set 4. a flip button 5. two arrows
+            to navigate the current set."""
+        ######################################
+
+        for each in setDict:
+            print(setDict)
+            print(setDict[each]) #this grabs the answers from a set
+
+        for widget in root.winfo_children(): #this clears the window
+            widget.destroy()
+
+        backBTN=Button(root, text="Return").pack(anchor=NW,side=LEFT,expand=YES,ipadx=25)
+        setTitle=Label(root,text=setName,width=50).pack(anchor=NW,side=LEFT,expand=YES,fill=X)
+        addBTN=Button(root, text="+").pack(anchor=NW,side=LEFT,expand=YES,ipadx=15)
+        subBTN=Button(root, text="-").pack(anchor=NW,side=LEFT,expand=YES,ipadx=15)
+
+        #here is where i left do the requirements for above ^
+    ####
 
 
 
@@ -26,24 +56,13 @@ class mainWindow():
         """This method will get the sets from another file and is called on init.
             OpenSet needs to be here as it needs to be defined before use"""
 
-        #####
-        def openSet(self,set):
-            """ when a set button is clicked this clears the screen and creates
-                1. return arrow 2. title of set, 3. plus and minus to create and
-                remove cards from the current set 4. a flip button 5. two arrows
-                to navigate the current set."""
-            ######################################
-            print("opening set:", loadedSets[set])
-            #here is where i left do the requirements for above ^
-        ####
-
         ##actual code
         with open('setCollectionFile', 'rb') as f:
             loadedSets = pickle.load(f)
             #print("unpickled", loadedSets)
             for set in loadedSets:  #this grabs the cardSet
 
-                Button(root, text=set, command= lambda set=set:openSet(self,set) ).pack(side=BOTTOM,expand=YES,fill=X)
+                Button(root, text=set, bd=5, command= lambda set=set:self.openSet(set,loadedSets[set],root) ).pack(anchor=N,side=BOTTOM,expand=YES,fill=BOTH)
 
 
 if __name__ == "__main__":
