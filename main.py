e248309e2847c38ff7b40470aa9ff3a14025fc45
SeanDavis268 <51014235+SeanDavis268@users.noreply.github.com>
2022-03-02 20:50:34 -0600

    Set Creation
    The ability to create new card sets is being worked on. Currently a new set can be made but not yet saved.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pickle
 
 class mainWindow():
     def __init__(self):
+
+
         self.createMenu()
 
 
@@ -95,6 +97,8 @@ class mainWindow():
         self.cardNumber=0
         self.flipped=False
 
+        self.workingTitle=""
+
         #these are only temporary
         nestedDict = {"does this work?":"I hope so", "yeet":"endSet1"}
         setCollection = { "Set1":nestedDict, "Set2":{"sq1":"sa1"}     }
@@ -116,18 +120,149 @@ class mainWindow():
         #NOTE also need anchored
         self.QAtext=StringVar(win,"oof")
         headerTxt = Label(win, text="Flash Cards").pack(side=TOP)
-        newSetBTN = Button(win, text="create new set").pack(anchor=N,side=TOP,expand=YES,fill=X)
+        newSetBTN = Button(win, text="create new set", command= lambda:self.createSet(win)).pack(anchor=N,side=TOP,expand=YES,fill=X)
 
 
         self.populate(win)
         win.mainloop()
 
-    def addCard(self):
-        #needs to function both in card view and when creating new sets
-        pass
+    class addCard():
+
+        def __init__(self,root,Q,A):
+            #then adds QA into a cardList for easy viewing
+            self.root=root
+            self.question=Q
+            self.answer=A
+            self.newCardFrame=Frame(root, bg="white",bd=3)
+            self.newCardCanvas=Canvas(self.newCardFrame, bg="lightblue")
+            self.newCardText=Label(self.newCardCanvas,text="Question:"+Q, bg="lightblue",padx=10)
+            self.newCardText.grid(column=0,row=0)
+            self.newCardText=Label(self.newCardCanvas,text="Answer:"+A, bg="lightblue",padx=10)
+            self.newCardText.grid(column=0,row=1)
+            self.newCardViewBTN=Button(self.newCardCanvas,text="View")
+            self.newCardViewBTN.grid(column=1,row=0)
+            self.newCardDelBTN=Button(self.newCardCanvas,text="Del",command= lambda:self.killself())
+            self.newCardDelBTN.grid(column=1,row=1)
+
+            self.newCardCanvas.pack()
+            self.newCardFrame.pack()
+        def killself(self):
+            #clear data and remove from list
+            for each in self.newCardFrame.winfo_children():
+                each.destroy()
+
+            self.newCardFrame.pack_forget()
+            #now to remove the qa from the list 
+
+
+
+
     def subCard(self):
         #same as above needs to work in both card view and creating sets
         pass
+
+    def viewCard(self):
+        pass
+
+
+
+    def createSet(self,root):
+
+        self.workingQ=""
+        self.workingA=""
+        self.workingDict={}
+
+        """clears screen and provides two entry boxes (question/answer) along with
+           a button to add them to the set. Along with a button to return to menu """
+        for widget in root.winfo_children(): #this clears the window
+            widget.destroy()
+
+        #now create the entry boxes with labels
+        backBTN=Button(text="Return",command= lambda:self.createMenu(root)).grid(column=0,row=0,ipadx=25)
+
+        if self.workingTitle=="":
+            print("first phase")
+            #if a title has not been made
+            titleEntryLabel= Label(text="Enter Title of Set Here:")
+            titleEntryLabel.grid(column=1,row=1)
+
+            def getTitle():
+                #check if empty or if just whiteSpace
+                if titleEntry.get().strip()!="":
+                    self.workingTitle=titleEntry.get().strip()
+                    print(self.workingTitle)
+                    self.createSet(root)
+
+            titleEntry= Entry()
+            titleEntry.grid(column=2,row=1)
+            titleEntry.bind("<Return>",(lambda event: getTitle()))
+
+            enterBTN=Button(text="Enter", command= getTitle) #will need command
+            enterBTN.grid(column=3,row=1)
+
+        else:
+            #after the title has been chosen
+            titleLabel= Label(text= "Set Title: "+ self.workingTitle)
+            titleLabel.grid(column=1,row=1)
+            Qlabel= Label(text="Question:")
+            Qlabel.grid(column=0,row=3,ipady=20)
+            Qinput= Entry()
+            Qinput.grid(column=1,row=3)
+
+            Alabel=Label(text="Answer:")
+            Alabel.grid(column=2,row=3,ipady=20, ipadx=10)
+            Ainput= Entry()
+            Ainput.grid(column=3,row=3)
+
+            #grab,record,pack
+            def grabQA():
+                 #enters QA into the workingDict
+                 if Qinput.get().strip()!= "" and Ainput.get().strip():
+                     print("valid", self.workingDict)
+                     questionStrip= Qinput.get().strip()
+                     answerStrip=Ainput.get().strip()
+
+                     if questionStrip not in self.workingDict:
+                         self.workingDict[questionStrip]= answerStrip
+                         self.addCard(cardSubFrame,questionStrip,answerStrip)
+
+                         cardSubFrame.update_idletasks()
+                         cardCanvas.config(scrollregion=cardCanvas.bbox("all"))
+                         #now make new key/value card and add it to a scrollable view
+
+
+            ############
+            QAEnterBTN= Button(text="Enter", command=grabQA)
+            QAEnterBTN.grid(column=3,row=4)
+            QAEnterBTN.bind("<Return>",(lambda event: grabQA() ))
+
+
+            cardFrame= Frame(root)
+            cardFrame.grid(column=1,row=5,columnspan = 4,sticky="nw")
+            cardFrame.grid_rowconfigure(0, weight=1)
+            cardFrame.grid_columnconfigure(0, weight=1)
+
+            cardCanvas=Canvas(cardFrame)
+            cardCanvas.grid(column=0,row=0, sticky="nw")
+
+            scroll= Scrollbar(cardFrame, orient="vertical", command=cardCanvas.yview)
+            scroll.grid(column=0,row=1,stick="ns")
+            cardCanvas.configure(yscrollcommand=scroll.set)
+
+            cardSubFrame=Frame(cardFrame,bg="white")
+            cardCanvas.create_window((0,0), window=cardSubFrame, anchor="nw")
+
+            #for x in range(20):
+            #    print("oof")
+            #    Label(cardSubFrame,text="hello world").grid(column=0,row=x+4)
+
+
+
+            cardSubFrame.update_idletasks()
+            cardFrame.config(width=200,height=200)
+            cardCanvas.config(scrollregion=cardCanvas.bbox("all"))
+
+
 
 
 
